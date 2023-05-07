793c8431fe2cee408bb3300bb7d37b8a8829516f
Timotej <tim.suste@gmail.com>
2023-05-07 20:24:42 +0200

    IRCode 50%

--- src/compiler/ir/IRCodeGenerator.java
+++ src/compiler/ir/IRCodeGenerator.java
@@ -15,6 +15,8 @@ import common.Report;
 import compiler.common.Visitor;
 import compiler.frm.Access;
 import compiler.frm.Frame;
+import compiler.frm.Access.Global;
+import compiler.frm.Access.Local;
 import compiler.frm.Frame.Label;
 import compiler.ir.chunk.Chunk;
 import compiler.ir.code.IRNode;
@@ -85,7 +87,16 @@ public class IRCodeGenerator implements Visitor {
             visit((Unary) expr);
     }
 
+    private IRNode getIRNode(Expr expr) {
+        this.parseExpr(expr);
+        var IRE = this.imcCode.valueFor(expr);
 
+        if (IRE.isPresent()) 
+            return (IRNode) IRE.get();
+        
+        Report.error(expr.position, "Epression" + expr.getClass().getName() + " code not found");
+        return null;
+    }
 
     public IRCodeGenerator(
         NodeDescription<IRNode> imcCode,
@@ -128,20 +139,28 @@ public class IRCodeGenerator implements Visitor {
     @Override
     public void visit(Binary binary) {
 
-        Operator op = Operator.valueOf(binary.operator.name());
-
         this.parseExpr(binary.left);
         this.parseExpr(binary.right);
 
         var lhs = imcCode.valueFor(binary.left);
         var rhs = imcCode.valueFor(binary.right);
 
-        if (lhs.isPresent() && rhs.isPresent()) {
-            var code = new BinopExpr((IRExpr) lhs.get(), (IRExpr) rhs.get(), op);
-            imcCode.store(code, binary);
-        }
-        else 
+        var Lmem = new MemExpr((IRExpr) lhs.get());
+        var Rmem = new MemExpr((IRExpr) rhs.get());
+
+        if (!(lhs.isPresent() && rhs.isPresent()))
             Report.error(binary.position, "Binary expression isn't present");
+
+        if (binary.operator == compiler.parser.ast.expr.Binary.Operator.ASSIGN) {
+            
+            var move = new MoveStmt((IRExpr) lhs.get(), Rmem);
+            imcCode.store(move, binary);  
+        }
+        else {            
+            Operator op = Operator.valueOf(binary.operator.name());        
+            var code = new BinopExpr(Lmem, Rmem, op);
+            imcCode.store(code, binary);        
+        }
     }
 
     @Override
@@ -152,26 +171,173 @@ public class IRCodeGenerator implements Visitor {
 
     @Override
     public void visit(For forLoop) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+                 
+        var stavki = new ArrayList<IRStmt>();
+
+        var L0 = new LabelStmt(Label.nextAnonymous());
+        var L1 = new LabelStmt(Label.nextAnonymous());
+        var L2 = new LabelStmt(Label.nextAnonymous());
+
+        var counter = this.getIRNode(forLoop.counter);
+        var low = this.getIRNode(forLoop.low);
+        var high = this.getIRNode(forLoop.high);               
+
+        // assign 
+        var LowMem = new MemExpr((IRExpr) low);
+        var move = new MoveStmt((IRExpr) counter, LowMem);
+        stavki.add(move);
+
+        // start
+        stavki.add(L0);
+
+        // condition
+        var CounterMem = new MemExpr((IRExpr) counter);
+        var HighMem = new MemExpr((IRExpr) high);
+
+        var bin = new BinopExpr(CounterMem, HighMem, Operator.LEQ);
+        var cjump = new CJumpStmt(bin, L1.label, L2.label);
+        stavki.add(cjump);        
+
+        // body        
+        stavki.add(L1);
+
+        var body = this.getIRNode(forLoop.body);
+        stavki.add(new ExpStmt((IRExpr) body));
+
+        //step
+        var step = this.getIRNode(forLoop.step);
+        var StepMem = new MemExpr((IRExpr) step);
+
+        var binStep = new BinopExpr(CounterMem, StepMem, Operator.ADD);
+
+        var counterPlusStep = new MoveStmt((IRExpr) counter, binStep);
+        stavki.add(counterPlusStep);
+
+        // out
+        stavki.add(L2);
+
+        var code = new SeqStmt(stavki);
+        imcCode.store(code, forLoop);
     }
 
+    // tko da vedno vrne naslov in mors pol naknadno mem delat
     @Override
     public void visit(Name name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        var defined = this.definitions.valueFor(name);
+
+        if (!defined.isPresent())
+            Report.error(name.position, "Name nima definicije.");
+
+        var acc = this.accesses.valueFor(defined.get());
+
+        if (!acc.isPresent())
+            Report.error(name.position, "Name nima accessa.");
+
+       
+        if (acc.get() instanceof Global) {
+            var code = new NameExpr(((Global) acc.get()).label);
+            this.imcCode.store(code, name); 
+        }
+        else if (acc.get() instanceof Local) {
+            var dostop = (Local) acc.get();
+
+            var offset = new ConstantExpr(dostop.offset);
+
+            // TODO nevem kako bi dubu framepointer
+            IRExpr FP = new ConstantExpr(0);
+
+            var code = new BinopExpr(FP, offset, Operator.ADD);
+
+            this.imcCode.store(code, name); 
+        }
+        else if (acc.get() instanceof compiler.frm.Access.Parameter) {
+            var dostop = (compiler.frm.Access.Parameter) acc.get();
+
+            var offset = new ConstantExpr(dostop.offset);
+
+            // TODO nevem kako bi dubu framepointer Temp rabs uporabljat
+            IRExpr FP = new ConstantExpr(0);
+
+            var code = new BinopExpr(FP, offset, Operator.SUB);
+
+            this.imcCode.store(code, name); 
+        }
+        else 
+            Report.error(name.position, "Name is not global or local.?");
+        
     }
 
     @Override
     public void visit(IfThenElse ifThenElse) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+                
+        var stavki = new ArrayList<IRStmt>();
+
+        var L0 = new LabelStmt(Label.nextAnonymous());
+        var L1 = new LabelStmt(Label.nextAnonymous());
+        var L2 = new LabelStmt(Label.nextAnonymous());
+
+        // start
+        stavki.add(L0);
+
+        // condition
+        this.parseExpr(ifThenElse.condition);
+        var bin = imcCode.valueFor(ifThenElse.condition);
+        if (!bin.isPresent())
+            Report.error(ifThenElse.position, "While condition code not found.");
+
+        var cjump = new CJumpStmt((IRExpr) bin.get(), L1.label, L2.label);
+        stavki.add(cjump);
+
+        // then
+        stavki.add(L1);
+
+        this.parseExpr(ifThenElse.thenExpression);
+        var thenExpr = this.imcCode.valueFor(ifThenElse.thenExpression);
+
+        if (!thenExpr.isPresent())
+            Report.error(ifThenElse.position, "While body code not found.");
+
+        stavki.add(new ExpStmt((IRExpr) thenExpr.get()));
+
+        // else
+        stavki.add(L2);
+
+        if (ifThenElse.elseExpression.isPresent()) {
+
+            this.parseExpr(ifThenElse.elseExpression.get());
+            var elseExpr = this.imcCode.valueFor(ifThenElse.elseExpression.get());
+
+            if (!elseExpr.isPresent())
+                Report.error(ifThenElse.position, "While body code not found.");
+
+            stavki.add(new ExpStmt((IRExpr) elseExpr.get()));
+        }
+
+        var code = new SeqStmt(stavki);
+        imcCode.store(code, ifThenElse);
     }
 
     @Override
     public void visit(Literal literal) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        var type = this.types.valueFor(literal);
+
+        if (!type.isPresent())
+            Report.error(literal.position, "No literal type found.");
+        
+        IRNode code;
+        if (type.get().isInt())
+            code = new ConstantExpr(Integer.parseInt(literal.value));
+        else if (type.get().isLog()) {
+            if (literal.value == "true")
+                code = new ConstantExpr(1);
+            else 
+                code = new ConstantExpr(0);
+        }
+        // else if (type.get().isStr())
+            code = new ConstantExpr(0);
+         
+            
+        imcCode.store(code, literal);
     }
 
     @Override
@@ -196,8 +362,41 @@ public class IRCodeGenerator implements Visitor {
 
     @Override
     public void visit(While whileLoop) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        
+        var stavki = new ArrayList<IRStmt>();
+
+        var L0 = new LabelStmt(Label.nextAnonymous());
+        var L1 = new LabelStmt(Label.nextAnonymous());
+        var L2 = new LabelStmt(Label.nextAnonymous());
+
+        // start
+        stavki.add(L0);
+
+        // condition
+        this.parseExpr(whileLoop.condition);
+        var bin = imcCode.valueFor(whileLoop.condition);
+        if (!bin.isPresent())
+            Report.error(whileLoop.position, "While condition code not found.");
+
+        var cjump = new CJumpStmt((IRExpr) bin.get(), L1.label, L2.label);
+        stavki.add(cjump);
+
+        // then
+        stavki.add(L1);
+
+        this.parseExpr(whileLoop.body);
+        var body = this.imcCode.valueFor(whileLoop.body);
+
+        if (!body.isPresent())
+            Report.error(whileLoop.position, "While body code not found.");
+
+        stavki.add(new ExpStmt((IRExpr) body.get()));
+
+        // else
+        stavki.add(L2);
+
+        var code = new SeqStmt(stavki);
+        imcCode.store(code, whileLoop);
     }
 
     @Override
